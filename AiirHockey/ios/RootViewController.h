7927ad7f13e72ad55d272fc3a61676cf06803276
叶孙松 <yss@kanousunsongtekiMac-mini.local>
2013-08-28 08:52:27 +0800

    Initial Commit

---
+++ AiirHockey/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  AiirHockeyAppController.h
+//  AiirHockey
+//
+//  Created by 叶孙松 on 13-8-28.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
